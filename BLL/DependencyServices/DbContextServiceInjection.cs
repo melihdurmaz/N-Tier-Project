4d2740a1d8711cbaf9b4812dd6a82753563ea354
melih <durmazmelih.ml@gmail.com>
2024-07-16 14:20:50 +0300

    ConnecitionStiring Ve Bağlantı

---
+++ BLL/DependencyServices/DbContextServiceInjection.cs
@@ -0,0 +1,24 @@
+﻿using DAL.Contecxt;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DependencyServices
+{
+    public static class DbContextServiceInjection
+    {
+        public static IServiceCollection AddDbContextService(this IServiceCollection services)
+        {
+            ServiceProvider provider = services.BuildServiceProvider();
+            IConfiguration configuration = provider.GetService<IConfiguration>();
+            services.AddDbContextPool<MyContext>(options => options.UseSqlServer(configuration.GetConnectionString
+                ("MyConnecion")).UseLazyLoadingProxies());
+            return services;
+        }
+    }
+}
