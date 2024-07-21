0045e28b37a9ea1ea98dd9b4b1bdf35b68505732
melih <durmazmelih.ml@gmail.com>
2024-07-21 16:42:08 +0300

    Session tutacak bir sınıf oluşturdum

---
+++ _UI/Models/ExtensionClasses/SessionExtension.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace _UI.Models.ExtensionClasses
+{
+    public static class SessionExtension
+    {
+        public static void SetObject(this ISession session,string key, object obj)
+        {
+            string ObjectString=JsonConvert.SerializeObject(obj);
+            session.SetString(key, ObjectString);
+        }
+        public static T GetObject<T>(this ISession session,string key)where T : class 
+        {
+            string ObjectStiring=session.GetString(key);
+            if (ObjectStiring == null)
+            {
+                return null;
+            }
+           T deserializedObject=JsonConvert.DeserializeObject<T>(ObjectStiring);
+            return deserializedObject;  
+        }
+    }
+}
